01923fb9a770bbb0321100be4b342d3c68a813a7
Blair-White <blairjwhite@hotmail.com>
2021-02-26 11:24:36 -0500

    Reupload of init for first person game.

---
+++ MidtermGameJam/Assets/Scripts/Enemies/SkeleReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeleReceiver : MonoBehaviour
+{
+    public GameObject mParent;
+    void SkeleAttacked()
+    {
+        GameObject mplayer = GameObject.Find("Player");
+        mplayer.SendMessage("SkeleAttacked");
+    }
+    void hitFinished()
+    {
+        mParent.SendMessage("hitFinished");
+    }
+}
---
+++ MidtermGameJam/Assets/Scripts/Player/SwordController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour
+{
+    private GameObject mplayer;
+    public Animator animator;
+    public Material NormalMat, EmpoweredMat;
+    private bool isAttacking;
+    public bool checkHit;
+    public bool isWalking;
+    private float delayAttack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mplayer = GameObject.Find("Player");
+        animator.SetBool("PressedAttack", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isAttacking)
+        {
+            delayAttack += Time.deltaTime;
+            if (delayAttack > 1.5) { isAttacking = false; delayAttack = 0; }
+
+        }
+        if(!isAttacking)
+        if(InputManager.Instance.PressedMouse0())
+        {
+            isAttacking = true;
+            animator.SetBool("PressedAttack", true);
+        }
+    }
+
+    public void AttackFinished()
+    {
+        animator.SetBool("PressedAttack", false);
+        mplayer.SendMessage("AttackEnded");
+    }
+    public void AttackHit()
+    {
+        checkHit = true;
+        mplayer.SendMessage("Attacked");
+    }
+
+    private void Walking()
+    {
+        animator.SetBool("isWalking", true);
+        isWalking = true;
+    }
+    private void StoppedWalking()
+    {
+        animator.SetBool("isWalking", false);
+        isWalking = false;
+    }
+
+    void SetEmpoweredMat() { this.GetComponent<MeshRenderer>().material = EmpoweredMat; }
+    void SetNormalMat() { this.GetComponent<MeshRenderer>().material = NormalMat; }
+}
+
