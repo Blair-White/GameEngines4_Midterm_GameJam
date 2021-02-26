73527bd335203ebc9cfa1dc24b9a6d9d801ff997
Blair-White <blairjwhite@hotmail.com>
2021-02-26 11:38:46 -0500

    First Setup of player Controller.

--- MidtermGameJam/Assets/Scripts/Player/PlayerController.cs
+++ MidtermGameJam/Assets/Scripts/Player/PlayerController.cs
@@ -5,16 +5,9 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    //clean this all up if time.
-    public GameObject HealthBar, ExpBar, oHitFlash, LevelupEffect, LevelUpUI, Sword, ShieldedBar, UiController, level1, level2, faceeffectlocation, regenEffect, HitEffect;
-    private float health, exp, totalExpNeeded, expCalc, targetexpCalc, targetHealth, flashdelay, mDamage;
-    private float StoneSkinReduction, giftcooldown, regenAmount, regenCount;
-    public float[] levelupExps;
-    private int myLevel, empowermultiplier, prayercharges;
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer, hitFlash, empowered, shielded;
-    public bool AttackFrame;
+    private bool groundedPlayer;
     [SerializeField]
     private float playerSpeed = 2.0f;
     [SerializeField]
@@ -23,104 +16,18 @@ public class PlayerController : MonoBehaviour
     private float gravityValue = -9.81f;
     private InputManager inputManager;
     private Transform cameraTransform;
-    private int iAttackcount;
-    private Vector3 lastPos;
     //talent bools
-    public bool StoneSkin, Poison, Gift, Prayer, Regeneration, Runes;
     private void Start()
     {
-        myLevel = 1;
-        totalExpNeeded = levelupExps[myLevel];
-        expCalc = 0; targetexpCalc = 0; exp = 0;
-        health = 100; targetHealth = 100;
+
         controller = GetComponent<CharacterController>();
         inputManager = InputManager.Instance;
         cameraTransform = Camera.main.transform;
-        mDamage = 25f;
-        StoneSkinReduction = 0;
-        level1 = GameObject.Find("LevelUp1"); level2 = GameObject.Find("LevelUp2");
-        level1.SetActive(false); level2.SetActive(false);
-        empowermultiplier = 2;
-        giftcooldown = .5f;
-        prayercharges = 1;
-        regenAmount = 5;
     }
 
-    private void FixedUpdate()
-    {
-        if (hitFlash)
-        {
-            flashdelay += Time.deltaTime;
-            if (flashdelay > 0.05f)
-            {
-                oHitFlash.SetActive(false);
-                hitFlash = false;
-                flashdelay = 0;
-            }
 
-        }
-    }
-    private void LateUpdate()
-    {
-        
-
- 
-
-       
-
-    }
     void Update()
     {
-        if(Regeneration)
-        {
-            regenCount += Time.deltaTime;
-            if(regenCount > 3)
-            {
-                targetHealth += regenAmount;
-                regenCount = 0;
-                Instantiate(regenEffect, faceeffectlocation.transform);
-                Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.SpellHeal, .5f);
-            }
-        }
-        if (targetHealth > 100) targetHealth = 100;
-        if (targetHealth < 0) targetHealth = 0;
-        if (targetHealth <= 0) { SceneManager.LoadScene(4); }
-
-        Vector3 currentPos = transform.position;
-        if (currentPos != lastPos && !Sword.GetComponent<SwordController>().isWalking) Sword.SendMessage("Walking");
-        else
-        if (currentPos == lastPos && Sword.GetComponent<SwordController>().isWalking) Sword.SendMessage("StoppedWalking");
-        lastPos = currentPos;
-
-        if(health != targetHealth)
-        {
-            if (health > targetHealth)
-            {
-                health -= 0.1f;
-            }
-            if(health < targetHealth)
-            {
-                health += 0.1f;
-            }
-        }
-
-        if (expCalc != targetexpCalc)
-        {
-            if (expCalc > targetexpCalc)
-            {
-                expCalc -= 0.1f;
-            }
-            if (expCalc < targetexpCalc)
-            {
-                expCalc += 0.1f;
-            }
-        }
-
-        if (expCalc >= 100) LevelUp();
-
-        HealthBar.GetComponent<RectTransform>().sizeDelta = new Vector2(health, 99);
-        ExpBar.GetComponent<RectTransform>().sizeDelta = new Vector2(expCalc, 99);
-
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
@@ -131,13 +38,6 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(movement.x, 0f, movement.y);
         move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
         controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //if (move != Vector3.zero)
-        //{
-        //    gameObject.transform.forward = move;
-        //}
-
-        // Changes the height position of the player..
         if (inputManager.PlayerJumped() && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
@@ -147,102 +47,9 @@ public class PlayerController : MonoBehaviour
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
-    public void SkeleAttacked()
-    {
-        if(!shielded)
-        {
-            targetHealth -= (10 - StoneSkinReduction);
-            hitFlash = true;
-            oHitFlash.SetActive(true);
-            oHitFlash.GetComponent<Image>().color = Color.red;
-            Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.FleshImpact, .5f);
-        }
-        else
-        {
-            oHitFlash.SetActive(true);
-            hitFlash = true;
-            oHitFlash.GetComponent<Image>().color = Color.cyan;
-            if (shielded)
-            {
-                if (Prayer) if (prayercharges == 0) { Unshield();  return; }
-                if (Prayer) if (prayercharges > 0) { prayercharges--; return; }
-                if (!Prayer) { Unshield(); }
-                Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.StepArmor, .5f);
-            }
-                
-        }
-    }
 
-    void SkeleHit(GameObject enemy)
-    {
-        float applyDamage;
-        if (!empowered) applyDamage = mDamage; else applyDamage = mDamage * empowermultiplier;
 
-        if (AttackFrame == true)
-        {
-            AttackFrame = false;
-            enemy.SendMessage("GotHit", applyDamage, SendMessageOptions.RequireReceiver);
-            if (empowered) UnEmpower();
-            Instantiate(HitEffect, enemy.transform);
-            Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.MonsterHit, .5f);
-            Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.BoneImpact, .6f);
-        }
 
-        if(Poison)
-        {
-            enemy.SendMessage("Poisoned");
-            //Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.MonsterHit, .5f);
-        }
 
-        
-    }
-
-    void Attacked()
-    {
-        AttackFrame = true;
-        Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.SwordSwing, .5f);
-    }
-    void AttackEnded()
-    {
-        AttackFrame = false;
-    }
-    
-    public void GrantExp(float granted)
-    {
-        exp += granted;
-        targetexpCalc = exp / totalExpNeeded * 100;
-    }
-
-    private void Empower() { empowered = true; Sword.SendMessage("SetEmpoweredMat"); }
-
-    private void UnEmpower() { empowered = false; Sword.SendMessage("SetNormalMat"); UiController.SendMessage("EmpowerEnded"); }
-    private void ShieldMe() { shielded = true; ShieldedBar.SetActive(true); if (Prayer) prayercharges = 1; Soundmanager.instance.PlaySoundOneShot(Soundmanager.instance.SpellAir, .5f); }
-    private void Unshield() { shielded = false; ShieldedBar.SetActive(false); UiController.SendMessage("ShieldEnded");prayercharges = 1; }
-
-    private void LevelUp()
-    {
-        Cursor.visible = true;
-        //temp system to get it just done quick
-        if (myLevel == 1) { level1.SetActive(true); }
-        if(myLevel == 2) { level2.SetActive(true); }
-        myLevel++;
-        exp = 0;
-        targetexpCalc = 0;
-        totalExpNeeded = levelupExps[myLevel];
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "EndPortal")
-        {
-            SceneManager.LoadScene(3);
-        }
-    }
-    public void PoisonSelect() { Poison = true; level1.SetActive(false); Cursor.visible = false; }
-    public void StoneSkinSelect() { StoneSkin = true; StoneSkinReduction = 2.5f; level1.SetActive(false); Cursor.visible = false; }
-    public void GiftSelect() { Gift = true; level1.SetActive(false); Cursor.visible = false; UiController.SendMessage("GiftSelect"); }
-    public void PrayerSelect() { Prayer = true; level2.SetActive(false); Cursor.visible = false; }
-    public void RunesSelect() { Runes = true; level2.SetActive(false); Cursor.visible = false; empowermultiplier = 3; }
-    public void RegenerationSelect() { Regeneration = true; level2.SetActive(false); Cursor.visible = false; }
+   
 }
-// public bool StoneSkin, Poison, Gift, Prayer, Regeneration, Runes;
